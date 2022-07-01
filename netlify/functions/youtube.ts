e303a38fce402141f9efc79be3e5e6837ffe35e2
serinryu <serin0404@naver.com>
2022-07-02 02:20:20 +0900

    fix deploy

--- netlify/functions/youtube.ts
+++ netlify/functions/youtube.ts
@@ -7,7 +7,7 @@ export async function handler (event:APIGatewayEvent, context:Context) {
   console.log(event);
 
   const { query } : any = event.queryStringParameters || "";
-  const response = await fetch(`${YOUTUBE_SEARCH_ENDPOINT}?part=snippet&q=${query}&key=${process.env.YOUTUBE_API_KEY}&fields=items(id,snippet(channelId,title))&maxResults=1}`);
+  const response = await fetch(`${YOUTUBE_SEARCH_ENDPOINT}?part=snippet&q=${query}&key=${process.env.YOUTUBE_API_KEY}&fields=items(id,snippet(channelId,title))&maxResults=1`);
 
   console.log(query);
   console.log(response);
