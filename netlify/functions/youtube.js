39167a2d7ca5d0f13de2cd086934c2f9a7adaa28
serinryu <serin0404@naver.com>
2022-07-01 18:26:32 +0900

    fix deploy

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -8,7 +8,7 @@ exports.handler = async function (event, context) {
     const response = await axios.get(`${process.env.YOUTUBE_BASE_PATH}?part=snippet&q=${query}&key=${process.env.YOUTUBE_API_KEY}&fields=items(id,snippet(channelId,title))&maxResults=1}`);
     return {
       statusCode: 200,
-      body: JSON.stringify({ response: response }),
+      body: response,
     };
   } catch (err) {
     return {
