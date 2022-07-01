da1c75726e631b34319d3a15a538e72ef87c1180
serinryu <serin0404@naver.com>
2022-07-01 18:59:12 +0900

    fix deploy

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -1,16 +1,23 @@
+const YOUTUBE_SEARCH_ENDPOINT = "https://www.googleapis.com/youtube/v3/search";
+
 exports.handler = async function (event, context) {
   console.log(event);
   console.log(context);
   try {
     const { query } = event.queryStringParameters;
-    const response = await fetch(`${process.env.YOUTUBE_BASE_PATH}?part=snippet&q=${query}&key=${process.env.YOUTUBE_API_KEY}&fields=items(id,snippet(channelId,title))&maxResults=1}`);
+    const response = await fetch(`${YOUTUBE_SEARCH_ENDPOINT}?part=snippet&q=${query}&key=${process.env.YOUTUBE_API_KEY}&fields=items(id,snippet(channelId,title))&maxResults=1}`);
 
     console.log(query);
     console.log(response);
-
+    
+    const { statusCode, statusText, ok, headers } = response;
+    const body = JSON.stringify(await response.json());
     return {
-      statusCode: 200,
-      body: JSON.stringify(await response.json()),
+      statusCode,
+      statusText,
+      ok,
+      headers,
+      body,
     };
   } catch (err) {
     return {
